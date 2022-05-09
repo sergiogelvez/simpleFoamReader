44b3288b9f48f8c7a620c425e25a140a21e33944
Sergio Augusto Gélvez Cortés <sergio.gelvez@gmail.com>
2022-05-09 07:05:13 -0500

    agregué ejemplo del encabezado del archivo para más claridad

---
+++ header.cpp
@@ -0,0 +1,25 @@
+/*--------------------------------*- C++ -*----------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     | Website:  https://openfoam.org
+    \\  /    A nd           | Version:  dev
+     \\/     M anipulation  |
+\*---------------------------------------------------------------------------*/
+FoamFile
+{
+    format      binary;
+    class       volVectorField;
+    location    "0.1";
+    object      U;
+}
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+dimensions      [0 1 -1 0 0 0 0];
+
+internalField   nonuniform List<vector> 
+525520
+
+();
+
+    }
+}
